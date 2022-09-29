3e055f14b0009134622ce0fa8ffe6c57afc22321
Ihor <ihor.zhelezniak@mths.ca>
2022-09-29 13:54:17 +0000

    -

--- src/index.ts
+++ src/index.ts
@@ -10,17 +10,17 @@ import promptSync from 'prompt-sync'
 
 const prompt = promptSync()
 let e: number | undefined
+let mNumber: number | undefined
 const lightSpeed = 2.998 * Math.pow(10, 8)
 
 console.log('E=mc²')
 console.log('Light speed = 2.998*10⁸m/s')
 const mString = prompt('Enter M(kg): ')
 
-try {
-  let mNumber = parseInt(mString)
+if (mNumber = parseInt(mString)) {
   e = mNumber * Math.pow(lightSpeed, 2)
   console.log(`E = ${e}`)
-} catch (err) {
-  console.log('\nInvalid number!')
+} else {
+  console.log('\nInvalid input!')
 }
 console.log('\nDone.')
